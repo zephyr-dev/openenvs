b3ba8aabc8320bc5a4f59a4d69b104d2d775e68c
James Vanneman <zephyr-eng+james@gust.com>
2015-04-17 12:20:36 -0400

    github api interaction

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 --git ls-remote  git@heroku.com:zephyr-romeo.git
 --GET /repos/:owner/:repo/commits/:sha
 
@@ -6,13 +7,17 @@
 --
 --
 --
+import Network.Wreq(getWith, defaults, basicAuth, auth, responseBody)
 import System.Process(readProcessWithExitCode)
+import Control.Lens((.~), (&), (^.), (?~))
 import Data.List.Split(splitOn)
+import qualified Data.ByteString.Lazy as BS
 
 type GitUrl = String
 type SHA = String
 
 data Environment = Environment { lastSHA :: String, gitUrl :: GitUrl, environmentName :: String }
+opts = defaults & auth ?~ basicAuth "zephyr-dev@googlegroups.com" ""
 
 instance Show Environment where
   show (Environment _ url name) = name ++ ": " ++ url
@@ -37,5 +42,5 @@ lastDeployedSha gitUrl  = do
 
 main :: IO ()
 main = do 
-  ld <- lastDeploys
-  mapM_ print ld
+  res <- getWith opts "https://api.github.com/repos/zephyr-dev/gust/git/commits/77f5947728753cb7afe6f0cfb9ae71a3597bf7e3"
+  BS.putStrLn $ res ^. responseBody
