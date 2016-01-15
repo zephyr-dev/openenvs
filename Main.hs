3e0b1ebc8fbc1e3a736d19b5dc15f22bb2a3d60a
James Vanneman <jvannem@gmail.com>
2016-01-15 08:36:30 -0500

    WIP

--- Main.hs
+++ Main.hs
@@ -1,187 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Char(toLower)
-import Control.Monad.Trans(liftIO)
-import Control.Monad.Trans.Reader(runReaderT, ReaderT, ask)
-import Control.Applicative((<$>))
-import Data.Time.Format     (parseTime, formatTime)
-import Data.Time.Clock      (UTCTime)
-import Data.Time.Format(defaultTimeLocale)
-import System.Console.ANSI
-import qualified Network.HTTP.Types as NHT
-import Control.Exception as E
+import Control.Monad.Free(Free(..), liftF)
 import Network.HTTP.Conduit(HttpException(StatusCodeException) )
-import qualified Data.ByteString.Char8 as BCH
-import qualified Data.ByteString.Lazy as BL
 import Data.Aeson.Lens (_String, key)
+import Control.Applicative((<$>))
+import Data.List(splitAt)
+import System.Environment(getEnv)
 import Network.Wreq
-import System.Directory(getHomeDirectory, createDirectoryIfMissing, renameDirectory, doesDirectoryExist)
-import System.Environment
 import Control.Lens
-import Control.Monad((<=<))
-import qualified Data.Maybe as MB
-import qualified Text.Regex as TR
-import Control.Monad(liftM)
-import qualified Data.List as DL
+import Data.ByteString.Char8(pack)
+import Data.Text(unpack)
+
 import qualified Data.Text as T
-import System.Process(readProcessWithExitCode)
-import System.Exit(ExitCode(..))
-import qualified Control.Monad.Parallel as MP
+import Control.Exception as E
+data PivotalStory = PivotalStory { pivotalStoryStatus :: String } deriving Show
 
-type GitUrl = String
-type HerokuFolderPath = String
-type CommitMessage = String
-type SHA = String
-type StoryId = String
-type EnvironmentName = String
-
-data Environment = Environment { environmentName :: EnvironmentName, lastCommitter :: String, recentStories :: [PivotalStory] }
-data PivotalStory = PivotalStory { pivotalStoryStatus :: T.Text, storyUpdatedAt :: Maybe UTCTime } deriving Show
-
-instance Show Environment where
- show (Environment name lastCommitter stories)
-    | all storyAccepted stories = colorGreen $ name ++ ": " ++ read lastCommitter ++ storyStatuses stories
-    | otherwise                 = colorRed $ name ++ ": " ++ read lastCommitter ++ storyStatuses stories
-
-lowerString :: [Char] -> [Char]
-lowerString = map toLower
-
-colorGreen string = setSGRCode [SetColor Foreground Dull Green] ++ string ++ "\x1b[0m"
-colorRed string = setSGRCode [SetColor Foreground Dull Red] ++ string ++ "\x1b[0m"
-
-storyStatuses :: [PivotalStory] -> String
-storyStatuses xs = let dateString = if (length pendingAcceptance > 0)  then
-                                      "since " ++ formattedLastUpdatedDate
-                                      else "" in
-                    " " ++ show (length pendingAcceptance) ++ " stories pending acceptance "  ++ dateString where
-  formattedLastUpdatedDate = MB.maybe "" id $ formatTime defaultTimeLocale "%D" <$> lastUpdatedDate
-  lastUpdatedDate = MB.listToMaybe . reverse . DL.sort  . MB.catMaybes $ map storyUpdatedAt pendingAcceptance
-  pendingAcceptance = filter (not . storyAccepted) xs
-
-
-gitUrlFor :: String -> GitUrl
-gitUrlFor envName = "git@heroku.com:zephyr-" ++ lowerString envName ++ ".git"
-
-deprecatedHerokuFolderPath = "/Users/gust/workspace/heroku_envs/"
-
-mkDir folderName = readProcessWithExitCode "mkdir" [folderName] ""
-
-fileNameForEnv :: String -> String
-fileNameForEnv name = "zephyr-" ++ lowerString name
-
-pullRepo :: EnvironmentName -> ReaderT HerokuFolderPath IO ExitCode
-pullRepo name = do
-  herokuFolderPath <- ask
-  (exitStatus, _, _) <- liftIO $ readProcessWithExitCode "git" ["-C", herokuFolderPath ++ (fileNameForEnv name), "pull", "-s", "ours", "--rebase"] ""
-  return exitStatus
-
-cloneRepo :: EnvironmentName -> ReaderT HerokuFolderPath IO ()
-cloneRepo name = do
-  let url = gitUrlFor name
-  herokuFolderPath <- ask
-  liftIO $ putStrLn $ "Creating a local copy of: " ++ name ++ " in " ++ herokuFolderPath ++ fileNameForEnv name ++ " this might take a minute"
-
-  (exitStatus, _, _) <- liftIO $ readProcessWithExitCode "git" ["-C", herokuFolderPath, "clone", url] ""
-  case exitStatus of 
-    ExitSuccess -> return()
-    ExitFailure x -> liftIO . putStrLn $ "The command " ++ "'git -C " ++ herokuFolderPath ++ " clone " ++ url ++ "' failed!"
-
-hasLocalCopyOfRepo :: String -> ReaderT HerokuFolderPath IO Bool
-hasLocalCopyOfRepo name = do
-  herokuFolderPath <- ask
-  (_, localDirContents, _) <- liftIO $ readProcessWithExitCode "ls" [herokuFolderPath] ""
-  return $ T.isInfixOf (T.pack $ fileNameForEnv name) (T.pack localDirContents)
-
-checkRepo name = do
-  herokuFolderPath <- ask
-  isTrue <- hasLocalCopyOfRepo name
-  if isTrue then do
-    pullStatus <- pullRepo name
-    let fixMessage = "cd " ++ herokuFolderPath ++ " && git status\n" ++ "If there is a merge conflict abort the merge and try again. If there is uncommited work remove it and try again"
-    case pullStatus of
-      ExitSuccess -> checkEnvironmentStatus name
-      ExitFailure x -> liftIO . putStrLn $ "The command 'git -C " ++ herokuFolderPath ++ (fileNameForEnv name) ++ " pull -s ours --rebase' failed!"
-
-  else cloneRepo name >> checkEnvironmentStatus name
-
-lastCommitterName :: String -> ReaderT HerokuFolderPath IO String
-lastCommitterName environment = do
-  herokuFolderPath <- ask
-  (_, name,_) <- liftIO $ readProcessWithExitCode "git" ["-C", herokuFolderPath ++ fileNameForEnv environment, "show", "--format=format:\"%an\"", "-s"] ""
-  return name
-
-
-
-textToDate  :: String -> Maybe UTCTime
-textToDate  = parseTime defaultTimeLocale "%FT%X%QZ"
-
-pivotalStories :: [StoryId] -> ReaderT HerokuFolderPath IO [PivotalStory]
-pivotalStories storyIds = liftIO $ mapM getStory storyIds where
-  getStory :: StoryId -> IO PivotalStory
-  getStory storyId = do
-    apiToken <- liftM BCH.pack $ getEnv "PIVOTAL_TRACKER_API_TOKEN"
-    let options = defaults & header "X-TrackerToken" .~ [apiToken]
-    res <- tryRequest (getWith options $ "https://www.pivotaltracker.com/services/v5/stories/" ++ storyId)
-    case res of
-      Right response -> do
-        let updatedAt = textToDate . T.unpack $ response ^. responseBody . key "updated_at" . _String
-        let state =  response ^. responseBody . key "current_state" . _String
-        return $ PivotalStory state updatedAt
-      Left (StatusCodeException status headers _) -> do
-        case NHT.statusCode status of
-          -- We get a 403 when someone links to an epic
-          403 -> return $ PivotalStory "invalid_story_id" Nothing
-          404 -> return $ PivotalStory "invalid_story_id" Nothing
-          statusCode   -> do
-            putStrLn $ "Could not process request for story: " ++ storyId ++ ". openenvs received a " ++ show statusCode ++ " status from pivotal tracker. Defaulting to not accepted"
-            return $ PivotalStory "not_accepted" Nothing
-    where
-      tryRequest :: IO a ->  IO (Either HttpException a)
-      tryRequest = E.try
-
-storyAccepted :: PivotalStory -> Bool
-storyAccepted story = pivotalStoryStatus story == "accepted" || pivotalStoryStatus story == "invalid_story_id"
-
-parseStoryIds :: EnvironmentName -> ReaderT HerokuFolderPath IO [StoryId]
-parseStoryIds env = liftM storyIdsFromCommits (commitMessages env)
-
-storyIdsFromCommits :: [CommitMessage] -> [StoryId]
-storyIdsFromCommits = DL.nub . concat . (MB.mapMaybe parseStoryId)
-
-parseStoryId :: CommitMessage -> Maybe [StoryId]
-parseStoryId = TR.matchRegex (TR.mkRegex "#([0-9]+)")
-
-commitMessages :: EnvironmentName -> ReaderT HerokuFolderPath IO [CommitMessage]
-commitMessages env = do
-  messages <- mapM commitMessage [0..12]
-  return $ messages
-  where
-    commitMessage :: Int -> ReaderT HerokuFolderPath IO String
-    commitMessage commitNum = do
-      herokuFolderPath <- ask
-      (_, name,_) <- liftIO $ readProcessWithExitCode "git" ["-C", herokuFolderPath ++ fileNameForEnv env, "show", "HEAD~" ++ show commitNum, "--format=format:\"%s\"", "-s"] ""
-      return name
-
-
-analyzeCommits :: EnvironmentName -> ReaderT HerokuFolderPath IO Environment
-analyzeCommits environment = do
-  name <- lastCommitterName environment
-  stories <- (pivotalStories <=< parseStoryIds) environment
-  return $ Environment { lastCommitter = name, environmentName = environment, recentStories = stories }
-
-environmentNames = [ "Alpha", "Bravo", "Echo", "Delta", "Foxtrot", "Juliet", "Romeo", "Tango", "Whiskey" ]
-
-checkEnvironmentStatus :: EnvironmentName -> ReaderT HerokuFolderPath IO ()
-checkEnvironmentStatus envName = do
-  analyzeCommits envName >>= liftIO . putStrLn . show
+{- getStory :: String -> IO PivotalStory -}
+{- getStory storyId = do -}
+  {- token <- pack <$> getEnv "PIVOTAL_TRACKER_API_TOKEN" -}
+  {- let options = defaults & header "X-TrackerToken" .~ [token] -}
+  {- res <- E.try (getWith options $ "https://www.pivotaltracker.com/services/v5/stories/" ++ storyId) -}
+  {- case res of -}
+    {- Right response -> do -}
+      {- let story = PivotalStory $ unpack $ response ^. responseBody . key "current_state" . _String -}
+      {- print story -}
+      {- return story -}
+    {- Left (StatusCodeException status headers _) -> do -}
+      {- putStrLn $ "Could not process request for story: " ++ storyId ++ ". openenvs received a " ++ show status -}
+      {- return $ PivotalStory "not_status" -}
 
 main :: IO ()
 main = do
-  homeDir <- getHomeDirectory
-  let herokuFolderPath = homeDir ++ "/heroku_envs/"
-  hasDeprecatedFolderPath <- doesDirectoryExist deprecatedHerokuFolderPath
-  createDirectoryIfMissing False herokuFolderPath
-  if hasDeprecatedFolderPath then renameDirectory deprecatedHerokuFolderPath herokuFolderPath
-                             else return ()
-  flip runReaderT herokuFolderPath $ do
-    liftIO $ putStrLn "Checking environments"
-    MP.mapM checkRepo environmentNames >> return ()
+  interpretIO $ do
+    apiToken <- getEnv' "PIVOTAL_TRACKER_API_TOKEN"
+    storyId <- getLine'
+    getStory apiToken storyId
+  return ()
+
+
+interpretIO :: Free Interaction a -> IO ()
+interpretIO (Free (GetEnv s fn)) = getEnv s >>= interpretIO . fn
+interpretIO (Free (GetLine fn)) = getLine >>= interpretIO . fn
+interpretIO (Free (PrintF s n)) = print s >> interpretIO n
+interpretIO (Pure _)          = return ()
+
+type TrackerApi = Free Interaction
+data Interaction next = GetEnv String (String -> next) | GetLine (String -> next) | PrintF String next
+
+getEnv' :: String -> Free Interaction String
+getEnv' key = liftF $ GetEnv key id
+
+getLine' :: Free Interaction String
+getLine' = liftF $ GetLine id
+
+
+instance Functor Interaction where
+  fmap f (GetEnv s n) = GetEnv s (fmap f n)
+  fmap f (GetLine n) = GetLine (fmap f n)
+  fmap f (PrintF s n) = PrintF s (f n)
+
+
+printF :: String -> Free Interaction ()
+printF s = liftF $ PrintF s ()
+
+getStory :: String -> String -> TrackerApi ()
+getStory apiToken storyId = do
+                  printF apiToken
+                  printF storyId
+  {- let options = defaults & header "X-TrackerToken" .~ [token] -}
+  {- res <- E.try (getWith options $ "https://www.pivotaltracker.com/services/v5/stories/" ++ storyId) -}
+  {- case res of -}
+    {- Right response -> do -}
+      {- let story = PivotalStory $ unpack $ response ^. responseBody . key "current_state" . _String -}
+      {- print story -}
+      {- return story -}
+    {- Left (StatusCodeException status headers _) -> do -}
+      {- putStrLn $ "Could not process request for story: " ++ storyId ++ ". openenvs received a " ++ show status -}
+      {- return $ PivotalStory "not_status" -}
